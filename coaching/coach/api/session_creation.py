1d9ac0c236fef6f93aee45299812085c2fec187b
Reda Ben <redabendahou0@gmail.com>
2024-06-14 23:18:05 +0100

    categories

--- coaching/coach/api/session_creation.py
+++ coaching/coach/api/session_creation.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render
-from coach.models import session_location
+from coach.models import session_location, categorie
 
 def session_creation(request):
     
     locations=session_location.objects.all()
+    categories=categorie.objects.all()
     return render(
         request,
         'coach/session_creation.html',
-        {'locations':locations}
+        {'locations':locations, 'categorie':categories}
     )
