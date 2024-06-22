61d5de85fc94a1785948887833acc1dec5542130
Reda Ben <redabendahou0@gmail.com>
2024-06-22 19:49:24 +0100

    dashboard and profile

--- coaching/coach/api/profile.py
+++ coaching/coach/api/profile.py
@@ -7,6 +7,10 @@ def profile(request):
 
     for order in orders:
             for order_item in order.ordertoproduct_set.all():
+                if order_item.training_session:
+                    training_session_name = order_item.training_session.name
+                else:
+                    training_session_name = "No Session"
                 order_data.append({
                 "name":order_item.product_name,
                 "price":order_item.price,
@@ -14,6 +18,7 @@ def profile(request):
                 "last_name":order.last_name,
                 "email":order.email,
                 "order_total":order.order_total,
+                "training_session":training_session_name,
             })
 
     return render(request, 'coach/profile.html', {'product_data': order_data})
